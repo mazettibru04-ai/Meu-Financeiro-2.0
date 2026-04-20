b84b3ed6e1fc8da14143b9f29f4b1c574389d3d9
mazettibru04-ai <mazettibru04@gmail.com>
2026-04-20 04:27:06 -0400

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { 
-  getFirestore, collection, addDoc, onSnapshot 
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  doc,
+  updateDoc
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLSxAGB178Pm4EtWHLNXhE_xDj-XAuHFQ",
-  authDomain: "meu-fincaneiro.firebaseapp.com",
-  projectId: "meu-fincaneiro",
-  storageBucket: "meu-fincaneiro.firebasestorage.app",
+  authDomain: "meu-financeiro.firebaseapp.com",
+  projectId: "meu-financeiro",
+  storageBucket: "meu-financeiro.appspot.com",
   messagingSenderId: "836819309794",
   appId: "1:836819309794:web:7e0526e138dea6d2cebabb"
 };
@@ -15,4 +20,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, addDoc, onSnapshot };
+export {
+  db,
+  collection,
+  addDoc,
+  onSnapshot,
+  doc,
+  updateDoc
+};
